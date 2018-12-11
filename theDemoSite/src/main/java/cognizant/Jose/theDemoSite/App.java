117c83e5c653b2bed2b56c1fb563d6fe8412367e
Jose M <jmorenete@outlook.com>
2018-12-11 11:53:04 +0000

    first commit

---
+++ theDemoSite/src/main/java/cognizant/Jose/theDemoSite/App.java
@@ -0,0 +1,13 @@
+package cognizant.Jose.theDemoSite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ theDemoSite/src/test/java/cognizant/Jose/theDemoSite/basicTesting.java
@@ -0,0 +1,79 @@
+package cognizant.Jose.theDemoSite;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class basicTesting {
+	WebDriver driver;
+	WebDriver testD;
+	@Before
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/Users/jmmore/Desktop/chromedriver");
+		 driver = new ChromeDriver();
+		 
+		 driver.get("http://thedemosite.co.uk/addauser.php");
+			
+			WebElement userName = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input"));
+			WebElement password = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input"));
+			WebElement save = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input"));
+			userName.sendKeys("jjjj");
+			password.sendKeys("zzzz");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			save.click();
+
+			
+	}
+	
+	@Test
+	public void userLogin() {
+		
+		driver.manage().window().maximize();
+		driver.get("http://thedemosite.co.uk/login.php");
+		
+
+		WebElement userLog = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/p/input"));
+		WebElement passLog = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/p/input"));
+		WebElement login = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/p/input"));
+		
+		userLog.sendKeys("jjjj");
+		passLog.sendKeys("zzzz");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		login.click();
+		WebElement success = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b"));
+
+		
+		assertEquals("User logged in",true,success.isDisplayed());
+		
+	}
+	
+	
+	@After
+	public void teardown() {
+		try {
+			Thread.sleep(5000); //otherwise thread doeesnt sleep even if window appears to be closed.
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+}
+
