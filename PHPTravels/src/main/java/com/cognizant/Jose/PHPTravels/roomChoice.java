fe7d86ec2639531a690db1d6364fd0dd391ae825
Jose M <jmorenete@outlook.com>
2018-12-13 16:46:39 +0000

    4th commit

--- PHPTravels/src/main/java/com/cognizant/Jose/PHPTravels/roomChoice.java
+++ PHPTravels/src/main/java/com/cognizant/Jose/PHPTravels/roomChoice.java
@@ -3,18 +3,51 @@ package com.cognizant.Jose.PHPTravels;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 
 public class roomChoice {
 	
-	@FindBy(xpath ="//*[@id=\"ROOMS\"]/div") 
-	
-	public WebElement room;
-	public List<WebElement> rooms = room.findElements(By.tagName("div"));
+	@FindBys({
+		@FindBy(xpath ="//*[@id=\"ROOMS\"]/div"), 
+		@FindBy(tagName = "div")
+	})
+	public List<WebElement> rooms;
 	
 	@FindBy(xpath = "//*[@id=\"ROOMS\"]/div/div[1]")
 	public WebElement scrollTo; 
 	
+	@FindBy(xpath = "//*[@id=\"ROOMS\"]/div/button")
+	public WebElement bookButton;
 	
+	public void hotelBooking() {
+		WebElement right = null;
+
+		 try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		 
+		 for(WebElement room:rooms) { 
+			String roomsClass = room.getAttribute("class");
+			
+			if(roomsClass.equals("col-md-10 col-xs-7 g0-left")) {
+				String text = room.findElement(By.tagName("small")).getText();
+				if(text.contains("ADULTS 3")) {
+					right = room;
+					break;
+				}
+				continue;
+			}
+		 }
+		 WebElement button = right.findElement(By.className("control__indicator"));
+		 button.click();
+		 bookButton.click();
+	}
+//	@FindBy()
+//	public WebElement ki;
 }
--- PHPTravels/src/test/java/com/cognizant/Jose/PHPTravels/SeleniumTest.java
+++ PHPTravels/src/test/java/com/cognizant/Jose/PHPTravels/SeleniumTest.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class seleniumTest {
+public class SeleniumTest {
 	WebDriver driver;
 	@Before
 	public void setup() {
@@ -27,40 +27,28 @@ public class seleniumTest {
 	}
 	
 	@Test
-	public void hotelBooking() {
+	public void testBooking() {
 		 
 		 driver.get("https://www.phptravels.net/hotels/detail/London/Grand-Plaza-Apartments");
 		 Actions action  = new Actions(driver);
-
-		 List<WebElement> rooms = driver.findElement(By.xpath("//*[@id=\"ROOMS\"]/div")).findElements(By.tagName("div"));
-		 WebElement scrollTo = driver.findElement(By.xpath("//*[@id=\"ROOMS\"]/div/div[1]"));
-		 WebElement bookButton = driver.findElement(By.xpath("//*[@id=\"ROOMS\"]/div/button"));
-		 WebElement right = null;
-		 
-		 try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
+//
+//		 List<WebElement> rooms = driver.findElement(By.xpath("//*[@id=\"ROOMS\"]/div")).findElements(By.tagName("div"));
+//		 WebElement scrollTo = driver.findElement(By.xpath("//*[@id=\"ROOMS\"]/div/div[1]"));
+//		 WebElement bookButton = driver.findElement(By.xpath("//*[@id=\"ROOMS\"]/div/button"));
+//		 WebElement right = null;
+//		 
+//		 try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e1) {
+//			e1.printStackTrace();
+//		}
 		 action.moveToElement(scrollTo).perform();
 		 action.sendKeys(Keys.PAGE_DOWN).perform();
 		 
-		 for(int i=0;i<rooms.size();i++) { 
-			String roomsClass = rooms.get(i).getAttribute("class");
-			
-			if(roomsClass.equals("col-md-10 col-xs-7 g0-left")) {
-				String text = rooms.get(i).findElement(By.tagName("small")).getText();
-				if(text.contains("ADULTS 3")) {
-					right = rooms.get(i);
-					break;
-				}
-				continue;
-			}
-		}
 
-		 WebElement button = right.findElement(By.className("control__indicator"));
-		 button.click();
-		 bookButton.click();
+//		 WebElement button = right.findElement(By.className("control__indicator"));
+//		 button.click();
+//		 bookButton.click();
 		 
 		 WebElement firstName = driver.findElement(By.xpath("//*[@id=\"guestform\"]/div[1]/div[2]/input"));
 		 WebElement lastName = driver.findElement(By.xpath("//*[@id=\"guestform\"]/div[1]/div[3]/input"));
