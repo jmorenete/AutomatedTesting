249e7dd307ec1f71fbd5c301419c216ad3ff72c5
Jose M <jmorenete@outlook.com>
2018-12-13 17:03:14 +0000

    Reports added

--- bddparameterisation/src/test/java/com/cognizant/jose/bddparameterisation/Step.java
+++ bddparameterisation/src/test/java/com/cognizant/jose/bddparameterisation/Step.java
@@ -30,55 +30,64 @@ public class Step {
 	static ExtentReports reports;
 	ExtentTest test;
 	String searchTerm;
-	
+	String title="";
+
 	@Before
 	public void setup() {
-		if(counter==0) {
-			reports = new ExtentReports("/Users/jmmore/Desktop/Report_test/reportParam.html");
+		if (counter == 0) {
+			reports = new ExtentReports("/Users/jmmore/Desktop/Report_test/reportParam.html", true);
 		}
-		
+
 		System.setProperty("webdriver.chrome.driver", "/Users/jmmore/Desktop/chromedriver");
 		driver = new ChromeDriver();
 		counter++;
 	}
-	
+
 	@After
 	public void teardown() {
 		reports.endTest(test);
 		reports.flush();
 		driver.quit();
-				
-		if(counter == 4) {
+
+		if (counter == 4) {
 			reports.close();
 		}
 	}
-	
+
 	@Given("^I go to \"([^\"]*)\" website$")
 	public void i_go_to_website(String arg1) throws Exception {
-		String title="";
-		switch(counter) {
-		case(1):
+		switch (counter) {
+		case (1):
 			title = "First search";
-		case(2):
+			break;
+		case (2):
 			title = "Second search";
-		case(3):
+			break;
+		case (3):
 			title = "Third search";
-		case(4):
+			break;
+		case (4):
 			title = "Fourth search";
+			break;
 		}
-		
-	    test = reports.startTest(title);
+
+		test = reports.startTest(title);
 		driver.get(arg1);
-	    Thread.sleep(1000);
-	    String currentURL = driver.getCurrentUrl();
-	    
-	    if(currentURL.contains("bing")) {
-	    	test.log(LogStatus.PASS, "Right website returned");
-	    }
-	    else {
-	    	test.log(LogStatus.FAIL, "Wrong website returned");
-	    	fail();
-	    }
+		Thread.sleep(1000);
+		String currentURL = driver.getCurrentUrl();
+
+		try {
+			if (currentURL.contains("bing")) {
+				test.log(LogStatus.PASS, "Right website returned");
+			} 
+			else {
+				test.log(LogStatus.FAIL, "Wrong website returned");
+				fail();
+			}
+		}
+		catch(Exception e) {
+			fail();
+		}
 	}
 
 	@When("^I search for \"([^\"]*)\"$")
@@ -87,29 +96,28 @@ public class Step {
 		WebElement searchBox = driver.findElement(By.xpath("//*[@id=\"sb_form_q\"]"));
 		searchBox.sendKeys(arg1);
 		Thread.sleep(500);
-		
-		searchBox.submit();	   
+
+		searchBox.submit();
 	}
 
 	@Then("^I am taken to a list of data for that search$")
 	public void i_am_taken_to_a_list_of_data_for_that_search() throws Exception {
-	   List<WebElement> results = driver.findElement(By.id("b_results")).findElements(By.tagName("h2"));
-	   boolean itemFound = false;
-	  
-	   for(WebElement element :results) {
-		   if(element.getText().toLowerCase().contains(searchTerm)) {
-			   itemFound = true;
-			   break;
-		   }
-	   }
-	   
-	   if(itemFound) {
-		   test.log(LogStatus.PASS, "Search term is in the returned page");
-	   }
-	   else {
-		   test.log(LogStatus.FAIL, "Search term is NOT in the returned page");
-	   }
-	   
+		List<WebElement> results = driver.findElement(By.id("b_results")).findElements(By.tagName("h2"));
+		boolean itemFound = false;
+
+		for (WebElement element : results) {
+			if (element.getText().toLowerCase().contains(searchTerm)) {
+				itemFound = true;
+				break;
+			}
+		}
+
+		if (itemFound) {
+			test.log(LogStatus.PASS, "Search term: "+searchTerm+" is in the returned page");
+		} else {
+			test.log(LogStatus.FAIL, "Search term: "+searchTerm+" is NOT in the returned page");
+		}
+
 	}
 
 }
